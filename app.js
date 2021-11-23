c6caf15986ebd8edd7206abb94f31798115f6222
bobur1998brm <github1998brm@gmail.com>
2021-11-23 15:33:52 +0900

    number-brm

---
+++ app.js
@@ -0,0 +1,46 @@
+Number.prototype.brm = function(type, rest=2){
+    if(type === "int" || type === undefined){
+        let str, qol
+        if(String(this).indexOf(".") === -1){
+            str = String(this).split('').reverse().join('')
+            qol = ""
+        }else{
+            str = String(this).slice(0,String(this).indexOf(".")).split('').reverse().join('')
+            qol = rest === 0 ? "" : String(this).slice(String(this).indexOf(".")).slice(0, rest+1)
+        }
+        let arr = []
+        for(let i = 3; i < str.length + 3; i+=3){
+            arr.push(str.slice(i-3, i))
+        }
+        return arr.map(i=>i.split("").reverse().join("")).reverse().join(' ') + qol
+    }else if(type === "string"){
+        let leng = String(Math.floor(this)).length
+        let num = String(Math.floor(this)).split('').reverse()
+        if(leng < 4){
+            return Math.floor(this)
+        }else if(leng < 7){
+            return num.slice(3).reverse().join('')  + `${+num.slice(2,3) === 0 ? "" : "."+ num.slice(2,3)} k` 
+        }else if(leng < 10){
+            return num.slice(6).reverse().join('') + `${+num.slice(5,6) === 0 ? "" : "."+ num.slice(5,6)} m`
+        }else {
+            return num.slice(9).reverse().join('') + "B"
+        }
+    }
+}
+
+let h1 = document.querySelector('#h1')
+let form = document.querySelector("#form")
+let input = document.querySelector("#input")
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    h1.innerHTML = Number(input.value).brm("int", 3)
+    // h1.innerHTML = Number(input.value).brm("string")
+})
+
+
+
+// yarn add number-brm
+// npm i number-brm
+
+
